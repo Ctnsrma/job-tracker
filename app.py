66ebd5b1c6e89f3174bfedc7a76d73b34499f752
Ctnsrma <chetansharma98004@gmail.com>
2026-08-25 13:04:07 +0530

    Add base template, index page, and basic CSS

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 from config import Config
 
-load_dotenv()  # reads .env and loads values into os.environ
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 @app.route("/")
 def home():
-    return "Job Tracker is alive!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=app.config["DEBUG"])
